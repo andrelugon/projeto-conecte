06da2fd0b52ea00c2c5c71746ebc594e552fd841
andreluiz.lugon <andreluiz.lugon@gmail.com>
2020-07-14 15:01:35 -0300

    Restaura o app JC Contabilidade

--- jc_contabilidade/models.py
+++ jc_contabilidade/models.py
@@ -1,30 +1,18 @@
 from django.db import models
 
 class dado(models.Model):
-    nome_fantasia = models.CharField(max_length=100)
+    nome_fantasia = models.CharField(max_length=300)
     logomarca = models.ImageField(upload_to='logomarca', null=True, blank=True)
     endereço = models.CharField(max_length=300)
-    bairro = models.CharField(max_length=30)
-    cidade = models.CharField(max_length=30)
-    federações = (
-    ('AC', 'AC'), ('AL', 'AL'), ('AM', 'AM'), ('BA', 'BA'), ('CE', 'CE'), ('DF', 'DF'), ('ES', 'ES'), ('GO', 'GO'),
-    ('MA', 'MA'), ('MT', 'MT'), ('MS', 'MS'), ('MG', 'MG'), ('PA', 'PA'), ('PB', 'PB'), ('PR', 'PR'), ('PE', 'PE'),
-    ('PI', 'PI'), ('RJ', 'RJ'), ('RN', 'RN'), ('RS', 'RS'), ('RO', 'RO'), ('RR', 'RR'), ('SC', 'SC'), ('SP', 'SP'),
-    ('PI', 'PI'), ('SE', 'SE'), ('TO', 'TO'))
-    estado = models.CharField(max_length=50, null=True, blank=True, choices=federações)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
     cnpj = models.CharField(max_length=11)
     horario_de_atendimento = models.CharField(max_length=20)
     nome_do_proprietário = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100)
     email = models.EmailField()
-    telefone_fixo = models.CharField(max_length=11, null=True, blank=True)
+    telefone_fixo = models.CharField(max_length=11)
     celular_whatsapp = models.CharField(max_length=11)
-    anunciar = (
-        ('Produtos', 'Produtos'),
-        ('Serviços', 'Serviços'),
-        ('Produtos e serviços', 'Produtos e serviços'),
-    )
-    tenho_interesse_em_anunciar = models.CharField(max_length=50, null=True, blank=True, choices=anunciar)
     breve_descrição_da_empresa = models.TextField()
 
     def __str__(self):
