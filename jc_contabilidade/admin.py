ce7e77e3dcf9fd5dd1da32d333f73ce3e4654377
andreluiz.lugon <andreluiz.lugon@gmail.com>
2020-07-14 14:10:51 -0300

    incluindo a chave primária para dado/produto/serviço

--- jc_contabilidade/admin.py
+++ jc_contabilidade/admin.py
@@ -13,8 +13,7 @@ class serviçoAdmin(admin.ModelAdmin):
     list_display = ['nome_do_serviço', 'preço']
     search_fields = ['nome_do_serviço', 'preço']
 
-'''
+
 admin.site.register(dado, dadoAdmin)
 admin.site.register(produto, produtoAdmin)
 admin.site.register(serviço, serviçoAdmin)
-'''
