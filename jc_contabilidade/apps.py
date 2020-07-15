8f508ed8797520ba2f36c279716cfc6c38b26a00
andreluiz.lugon <andreluiz.lugon@gmail.com>
2020-07-14 21:38:55 -0300

    Remove o app JC Contabilidade

--- jc_contabilidade/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LgDistribuidoraConfig(AppConfig):
-    name = 'jc_contabilidade'
--- jc_contabilidade/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import home_jccontabilidade
-
-
-urlpatterns = [
-    path('', home_jccontabilidade),
-
-]
