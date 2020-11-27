07d25c2aed2311c9648e5199d8dc270eb874b7f1
andrelugon <andreluiz.lugon@gmail.com>
2020-11-27 18:12:23 -0300

    altera o tamanho do ícone pwa

--- app/settings.py
+++ app/settings.py
@@ -158,7 +158,7 @@ PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
         'src': '/static/images/site/icon.png',
-        'sizes': '150x150'
+        'sizes': '320x320'
     }
 ]
 PWA_APP_ICONS_APPLE = [
