1fc99e6ba122c2b9c2a194f1b39a518738cda4dc
andrelugon <andreluiz.lugon@gmail.com>
2020-11-27 21:11:41 -0300

    Altera o tamanho do ícone para abertura do app

--- app/settings.py
+++ app/settings.py
@@ -158,7 +158,7 @@ PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
         'src': '/static/images/site/icon.png',
-        'sizes': '400x400'
+        'sizes': '500x500'
     }
 ]
 PWA_APP_ICONS_APPLE = [
